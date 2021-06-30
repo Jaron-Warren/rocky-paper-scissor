f93691f44a11c5b35e8a754999bef344103b190e
Jaron-Warren <Jarwarren@gmail.com>
2021-06-30 15:50:36 -0600

    game finished

--- java.js
+++ java.js
@@ -1,1 +1,44 @@
-console.log('helloworld')
+
+// let choices = {
+//   rock: {
+//     name: "rock",
+//     beats: 'scissor'
+//   },
+//   scissors: {
+//     name: "scissors",
+//     beats: 'paper'
+//   },
+//   paper: {
+//     name: "paper",
+//     beats: 'rock'
+//   }
+// }
+let winConditions = []
+
+winConditions['paper'] = 'rock'
+winConditions['rock'] = 'scissors'
+winConditions['scissors'] = 'paper'
+
+function randomChoice() {
+  let random = Math.floor((Math.random() * 3) + 1)
+  switch (random) {
+    case 1:
+      return 'rock'
+    case 2:
+      return 'scissors'
+    case 3:
+      return 'paper'
+  }
+}
+
+function rockPaperScissor(playerChoice) {
+  let computerChoice = randomChoice()
+  console.log(computerChoice)
+  if (playerChoice == computerChoice) {
+    console.log('draw')
+  } else if (winConditions[playerChoice] == computerChoice) {
+    console.log('you win!')
+  } else console.log('you lose!')
+}
+
+
